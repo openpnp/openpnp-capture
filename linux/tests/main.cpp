7d5b7ca22b05e380911c3e0103bdad9757f2b48b
Niels Moseley <n.a.moseley@gmail.com>
2017-07-12 23:40:20 +0200

    Updated linux test program

--- linux/tests/main.cpp
+++ linux/tests/main.cpp
@@ -13,10 +13,34 @@
 #include "openpnp-capture.h"
 #include "../common/context.h"
 
+std::string FourCCToString(uint32_t fourcc)
+{
+    std::string v;
+    for(uint32_t i=0; i<4; i++)
+    {
+        v += static_cast<char>(fourcc & 0xFF);
+        fourcc >>= 8;
+    }
+    return v;
+}
+
 int main(int argc, char*argv[])
 {    
+    uint32_t deviceFormatID = 0;
+    uint32_t deviceID       = 0;
+
     printf("OpenPNP Capture Test Program\n");
     Cap_setLogLevel(7);
+
+    if (argc >= 2)
+    {
+        deviceID = atoi(argv[1]);
+    }
+    
+    if (argc >= 3)
+    {
+        deviceFormatID = atoi(argv[2]);
+    }
 
     CapContext ctx = Cap_createContext();
 
@@ -25,9 +49,25 @@ int main(int argc, char*argv[])
     for(uint32_t i=0; i<deviceCount; i++)
     {
         printf("ID %d -> %s\n", i, Cap_getDeviceName(ctx,i));
+
+        // show all supported frame buffer formats
+        int32_t nFormats = Cap_getNumFormats(ctx, i);
+
+        printf("  Number of formats: %d\n", nFormats);
+
+        std::string fourccString;
+        for(int32_t j=0; j<nFormats; j++)
+        {
+            CapFormatInfo finfo;
+            Cap_getFormatInfo(ctx, i, j, &finfo);
+            fourccString = FourCCToString(finfo.fourcc);
+
+            printf("  Format ID %d: %d x %d pixels  FOURCC=%s\n",
+                j, finfo.width, finfo.height, fourccString.c_str());
+        }
     }
 
-    int32_t streamID = Cap_openStream(ctx, 0, 0, 0, 0);
+    int32_t streamID = Cap_openStream(ctx, deviceID, deviceFormatID);
     printf("Stream ID = %d\n", streamID);
     
     if (Cap_isOpenStream(ctx, streamID) == 1)
@@ -37,12 +77,17 @@ int main(int argc, char*argv[])
     else
     {
         printf("Stream is closed (?)\n");
+        return 1;
     }
 
     printf("Press Q to exit..\n");
 
+    // get current stream parameters 
+    CapFormatInfo finfo;
+    Cap_getFormatInfo(ctx, deviceID, deviceFormatID, &finfo);
+
     std::vector<uint8_t> m_buffer;
-    m_buffer.resize(640*480*3);
+    m_buffer.resize(finfo.width*finfo.height*3);
 
     Cap_setAutoExposure(ctx, streamID, 1);
 
@@ -51,7 +96,7 @@ int main(int argc, char*argv[])
     uint32_t tries = 0;
     while(counter < 30)
     {
-        usleep(50000);
+        usleep(50*1000);
         printf("%d", Cap_getStreamFrameCount(ctx, streamID));
         if (Cap_hasNewFrame(ctx, streamID) == 1)
         {
@@ -76,14 +121,11 @@ int main(int argc, char*argv[])
         printf("Buffer captured!\n");
 
         FILE *fout = fopen("image.ppm", "wb");
-        fprintf(fout, "P6 640 480 255\n"); // PGM header
-
-        const uint32_t height = 480;
-        const uint32_t width  = 640;
+        fprintf(fout, "P6 %d %d 255\n", finfo.width, finfo.height); // PGM header
 
         // exchange BGR to RGB
         uint32_t idx = 0;
-        for(uint32_t i=0; i<width*height; i++)
+        for(uint32_t i=0; i<finfo.width*finfo.height; i++)
         {
             uint8_t b = m_buffer[idx];
             uint8_t g = m_buffer[idx+1];
@@ -95,10 +137,10 @@ int main(int argc, char*argv[])
 
         // and upside-down :)
         const uint32_t stride = 3;
-        const size_t lineBytes = width * stride;
+        const size_t lineBytes = finfo.width * stride;
         uint8_t *row  = new uint8_t[lineBytes];
         uint8_t *low  = &m_buffer[0];
-        uint8_t *high = &m_buffer[(height - 1) * lineBytes];
+        uint8_t *high = &m_buffer[(finfo.height - 1) * lineBytes];
 
         for (; low < high; low += lineBytes, high -= lineBytes) {
             memcpy(row, low, lineBytes);
