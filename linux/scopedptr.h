05a9acc874e7d85c693dec4532e6b98304b9e78d
Niels Moseley <n.a.moseley@gmail.com>
2017-07-17 17:36:47 +0200

    Added missing scopedptr.h to linux platform code

---
+++ linux/scopedptr.h
@@ -0,0 +1,84 @@
+/*
+
+    Scoped pointer to auto-delete the object
+    when it goes out of scope
+
+    Niels A. Moseley
+
+*/
+
+#ifndef scopedptr_h
+#define scopedptr_h
+
+/** The ScopedPtr takes a pointer to a generic object and will 
+    delete/destroy the object when the owning ScopedComPtr is 
+    released/destroyed.
+
+    If the pointer handled by ScopedComPtr needs to be returned
+    from a function, the Detach() method will transfer ownership.
+
+*/
+template <typename T> class ScopedPtr
+{
+public:
+    /** create a ScopedComPtr object and take ownership of
+        the Interface object/pointer */
+    ScopedPtr(T* ptr) : m_ptr(ptr)
+    {
+        
+    }
+
+    /** destroy the ScopedComPtr object and
+        release the associated Interface pointer */
+    ~ScopedPtr()
+    {
+        if (m_ptr != nullptr)
+        {
+            delete m_ptr;
+        }
+    }
+
+    /** access methods exposed by Interface */
+    T* operator->() const
+    {
+        return m_ptr;
+    }    
+
+    /** replace the internal pointer with a new one,
+        the old object is released/destroyed */
+    void Replace(T *newPtr)
+    {
+        InternalRelease();
+        m_ptr = newPtr;
+    }
+
+    /** detach the object from the scoped pointer object
+        without releasing it. */
+    T* Detach()
+    {
+        T *obj = m_ptr;
+        m_ptr = nullptr;
+        return obj;
+    }
+
+private:
+    void InternalRelease()
+    {
+        T * temp = m_ptr;
+        if (temp != nullptr)
+        {
+            m_ptr = nullptr;
+            delete temp;
+        }
+    }
+
+    //disallow copying of a scoped pointer!
+    ScopedPtr(ScopedPtr const & other)
+    {
+
+    }
+
+    T *m_ptr = nullptr;    
+};
+
+#endif
