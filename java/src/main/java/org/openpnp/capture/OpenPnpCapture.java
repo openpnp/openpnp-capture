70f1930fdcf9197c8c7736c65ca139c16fb50cc3
Jason von Nieda <jason@vonnieda.org>
2017-08-13 21:35:28 -0500

    Started to integrate Java support. JNAerator working for code generation. Got a basic test main in place that works on Mac, so far.

--- java/src/main/java/org/openpnp/capture/OpenPnpCapture.java
+++ java/src/main/java/org/openpnp/capture/OpenPnpCapture.java
@@ -1,46 +1,24 @@
 package org.openpnp.capture;
 
-import java.util.Arrays;
-import java.util.List;
+import org.openpnp.capture.library.OpenpnpCaptureLibrary;
 
+import com.sun.jna.Native;
 import com.sun.jna.Pointer;
-import com.sun.jna.ptr.IntByReference;
-import com.sun.jna.ptr.PointerByReference;
 
 public class OpenPnpCapture {
-    Pointer context;
-
-    public OpenPnpCapture() {
-        PointerByReference context = new PointerByReference();
-        OpenPnpCaptureLibrary.INSTANCE.create_context(context);
-        this.context = context.getValue();
-    }
-
-    public void release() {
-        if (context != null) {
-            OpenPnpCaptureLibrary.INSTANCE.release_context(context);
-            context = null;
-        }
-    }
-
-    public List<CaptureDevice> listDevices() {
-        PointerByReference pDevices = new PointerByReference();
-        IntByReference devicesLength = new IntByReference();
-        OpenPnpCaptureLibrary.INSTANCE.list_devices(context, pDevices, devicesLength);
-        CaptureDevice device = new CaptureDevice(pDevices.getValue());
-        device.read();
-        CaptureDevice[] devices = (CaptureDevice[]) device.toArray(devicesLength.getValue());
-        return Arrays.asList(devices);
-    }
-
+    // Library prefixes
+    // darwin
+    // win32-x86
+    // win32-x86-64
+    // linux-x86
+    // linux-x86-64
     public static void main(String[] args) {
         OpenPnpCapture capture = new OpenPnpCapture();
-        for (CaptureDevice device : capture.listDevices()) {
-            System.out.println(device);
-            for (CaptureFormat format : device.getFormats()) {
-                System.out.println("    " + format);
-            }
+        Pointer context = OpenpnpCaptureLibrary.INSTANCE.Cap_createContext();
+        int deviceCount = OpenpnpCaptureLibrary.INSTANCE.Cap_getDeviceCount(context);
+        for (int i = 0; i < deviceCount; i++) {
+            String deviceName = OpenpnpCaptureLibrary.INSTANCE.Cap_getDeviceName(context, i).getString(0);
+            System.out.println(String.format("%d %s", i, deviceName));
         }
-        capture.release();
     }
 }
